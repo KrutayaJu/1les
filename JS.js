602e26f50009f9142f87d2613005bc0f39674120
krutaya <krutaya_yulia@ukr.net>
2019-03-16 21:50:22 +0200

    initial commit

---
+++ JS.js
@@ -0,0 +1,63 @@
+// let a=1;
+// let b=3;
+// console.log(a+b);
+// console.log(b-a);
+// console.log(a*b);
+// console.log(b%a);
+// let c="привет";
+// let d="basic";
+// console.log(c-d);
+// console.log(c+d);
+
+// let a=prompt("fill a");
+// let b=prompt("fill b");
+// if (a>b) {
+//     alert(a);
+// }
+// else if (b>a) {
+//     alert(b);
+// }
+// else {
+//     alert(a+'='+b);
+// }
+// let a=prompt("fill a");
+// let b=prompt("fill b");
+//
+// if (a>1 && a<100 && b!=15){
+//     alert('hi');
+// }
+// // else {alert("no")};
+
+// let a=prompt("fill a");
+// let b=prompt("fill b");
+// if ((a>10 || b<15) && a!=b){
+//     alert('hi');
+// }
+//  else {alert("no")};
+
+// let a=prompt("fill a");
+// // let b=prompt("fill b");
+// // if ((a>10 || b<=12) && b>a){
+// //     alert('hi');
+// // }
+// // else {alert("no")};
+let a = [1, 2, 3, 4, 5, 6, 7];
+i = 0;
+for (i = 0; i < 7; i++) {
+    console.log(a[i])
+}
+
+i = 0;
+
+do {
+    console.log(a[i]);
+    i++
+}
+while (i < 7);
+
+i = 0;
+while (i < 7) {
+    console.log(a[i]);
+    i++
+}
+
